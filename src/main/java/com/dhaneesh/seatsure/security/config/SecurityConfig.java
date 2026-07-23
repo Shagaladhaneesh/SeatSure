8d30b40a2b5bb6ba27f04810b650ed42aa948c0e
Dhaneeshwor <shagaladhaneesh@gmail.com>
2026-07-23 21:54:42 +0530

    change in request matchers added / endpoint

--- src/main/java/com/dhaneesh/seatsure/security/config/SecurityConfig.java
+++ src/main/java/com/dhaneesh/seatsure/security/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "/",
                                 "/api/auth/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
